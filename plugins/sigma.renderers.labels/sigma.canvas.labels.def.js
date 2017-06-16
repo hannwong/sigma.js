ea9909da83e685894b3b78d8fff605f704de0093
Jon Wong <jhannwong@gmail.com>
2017-06-16 23:02:53 +0800

    Supports multiple labels, open arrow heads

--- plugins/sigma.renderers.labels/sigma.canvas.labels.def.js
+++ plugins/sigma.renderers.labels/sigma.canvas.labels.def.js
@@ -15,37 +15,56 @@
    * @param  {configurable}             settings The settings function.
    */
   sigma.canvas.labels.def = function(node, context, settings) {
-    var fontSize,
-        prefix = settings('prefix') || '',
-        size = node[prefix + 'size'];
-
-    if (size < settings('labelThreshold'))
+    if (node.label === undefined)
       return;
 
-    if (!node.label || typeof node.label !== 'string')
-      return;
-
-    fontSize = (settings('labelSize') === 'fixed') ?
-      settings('defaultLabelSize') :
-      settings('labelSizeRatio') * size;
-
-    if (node.labelSize !== undefined)
-      fontSize = node.labelSize;
-
-    context.font = (settings('fontStyle') ? settings('fontStyle') + ' ' : '') +
-      fontSize + 'px ' + settings('font');
-    context.fillStyle = (settings('labelColor') === 'node') ?
-      (node.color || settings('defaultNodeColor')) :
-      settings('defaultLabelColor');
-
-    var labelX = Math.round(node[prefix + 'x'] + size + 3);
-    var labelY = Math.round(node[prefix + 'y'] + fontSize / 3);
-    if (node.labelX !== undefined) {
-      labelX = Math.round(node[prefix + 'x'] + node.labelX);
+    // If node.label is not an array, make it a 1-element array.
+    var labels = node.label;
+    if (!Array.isArray(node.label)) {
+      labels = [];
+      labels.push(node.label);
     }
-    if (node.labelY !== undefined)
-      labelY = Math.round(node[prefix + 'y'] + node.labelY);
 
-    context.fillText(node.label, labelX, labelY);
+    var label;
+    for (var i = 0; i < labels.length; i++) {
+      label = labels[i];
+
+      var fontSize,
+          prefix = settings('prefix') || '',
+          size = node[prefix + 'size'];
+
+      if (size < settings('labelThreshold'))
+        return;
+
+      if (!label.label || typeof label.label !== 'string')
+        continue;
+
+      fontSize = (settings('labelSize') === 'fixed') ?
+        settings('defaultLabelSize') :
+        settings('labelSizeRatio') * size;
+
+      if (label.labelSize !== undefined)
+        fontSize = label.labelSize;
+
+      if (label.weight === undefined)
+        label.weight = '400';
+
+      context.font = (settings('fontStyle') ? settings('fontStyle') + ' ' : '') +
+        label.weight + ' ' +
+        fontSize + 'px ' + settings('font');
+      context.fillStyle = (settings('labelColor') === 'node') ?
+        (label.color || settings('defaultNodeColor')) :
+        settings('defaultLabelColor');
+
+      var labelX = Math.round(node[prefix + 'x'] + size + 3);
+      var labelY = Math.round(node[prefix + 'y'] + fontSize / 3);
+      if (label.labelX !== undefined) {
+        labelX = Math.round(node[prefix + 'x'] + label.labelX);
+      }
+      if (label.labelY !== undefined)
+        labelY = Math.round(node[prefix + 'y'] + label.labelY);
+
+      context.fillText(label.label, labelX, labelY);
+    }
   };
 }).call(this);
